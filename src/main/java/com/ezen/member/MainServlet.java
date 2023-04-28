7f28ff9f45e9efe71ba9871b79f3e80bcafc0efa
JAVA01 <JAVA01@DESKTOP-DJ2RLDE>
2023-04-28 17:58:00 +0900

    MAIN SERVLET 수정

--- src/main/java/com/ezen/member/MainServlet.java
+++ src/main/java/com/ezen/member/MainServlet.java
@@ -40,7 +40,7 @@ public class MainServlet extends HttpServlet {
 		//main.jsp로 가기 전 로그인이 되어있는 상태인지 먼저 점검
 		HttpSession session = request.getSession();
 		if(session.getAttribute("loginUser")==null) 
-		url = "member/loginUser.jsp"; // 로그인이 안된 상태이면 로그인 페이지로 경로를 변경
+		url = "member/loginForm.jsp"; // 로그인이 안된 상태이면 로그인 페이지로 경로를 변경
 		
 		MemberDao mdao = MemberDao.getInstance();
 		ArrayList<MemberDto> list = mdao.selectMember();
